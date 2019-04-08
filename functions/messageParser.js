6397e0f2b1150a6f7d027b18be1abb08dc8f8369
Jon <jondekerh@gmail.com>
2019-04-08 14:25:22 -0700

    made all commands case insensitive

--- functions/messageParser.js
+++ functions/messageParser.js
@@ -12,69 +12,69 @@ module.exports.parse = (msg, callout) => {
     var msgArr = msg.content.split(' ');
 
     //master switch for bot commands
-    switch (msgArr[1]) {
+    switch (true) {
       //say hi
-      case ('hello'):
+      case (/hello/i.test(msgArr[1])):
         msg.channel.send('Hello!');
       break;
       //help command
-      case ('help'):
+      case (/help/i.test(msgArr[1])):
         help.message(msg, callout);
       break;
       //setup command
-      case ('setup'):
+      case (/setup/i.test(msgArr[1])):
         setup.message(msg, callout);
       break;
       //initialize guild doc in DB
-      case ('init'):
+      case (/init/i.test(msgArr[1])):
         init.initialize(msg);
       break;
       //sets cooldown on post credit
-      case ('set-cooldown'):
+      case (/set-cooldown/i.test(msgArr[1])):
         set.cooldown(msg, msgArr);
       break;
       //sets posts to advance to granted role
-      case ('set-posts'):
+      case (/set-posts/i.test(msgArr[1])):
         set.posts(msg, msgArr);
       break;
       //sets days user must be in the server for granted role
-      case ('set-days'):
+      case (/set-days/i.test(msgArr[1])):
         set.days(msg, msgArr);
       break;
       //set bot command channel
-      case ('bot-channel'):
+      case (/bot-channel/i.test(msgArr[1])):
         set.channel(msg, msgArr, 'botChannel');
       break;
       //set greeting channel
-      case ('greeting-channel'):
+      case (/greeting-channel/i.test(msgArr[1])):
         set.channel(msg, msgArr, 'greetingChannel');
       break;
       //set rules channel
-      case ('rules-channel'):
+      case (/rules-channel/i.test(msgArr[1])):
         set.channel(msg, msgArr, 'rulesChannel');
       break;
       //set role to give members on join
-      case ('starting-role'):
+      case (/starting-role/i.test(msgArr[1])):
         set.role(msg, msgArr, 'startingRole');
       break;
       //set role to be granted after activity
-      case ('granted-role'):
+      case (/granted-role/i.test(msgArr[1])):
         set.role(msg, msgArr, 'grantedRole');
       break;
       //set role for muzzle
-      case ('muzzle-role'):
+      case (/muzzle-role/i.test(msgArr[1])):
         set.role(msg, msgArr, 'muzzleRole');
       break;
       //muzzle command
-      case ('muzzle'):
+      case (/muzzle/i.test(msgArr[1])):
         mod.muzzle(msg);
       break;
       //kick command
-      case ('kick'):
+      case (/kick/i.test(msgArr[1])):
         mod.kick(msg);
       break;
       //ban command
-      case ('ban'):
+      case (/ban/i.test(msgArr[1])):
         mod.ban(msg);
       break;
     };
