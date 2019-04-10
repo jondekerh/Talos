77462618572d284edfa5ed5106f267568bbfb811
Jon <jondekerh@gmail.com>
2019-04-10 13:29:28 -0700

    fixed small bug by using toUpperCase() instead of regex for case insensitivity

--- functions/messageParser.js
+++ functions/messageParser.js
@@ -11,70 +11,70 @@ module.exports.parse = (msg, callout) => {
     //create array so commands can be directed via switch
     var msgArr = msg.content.split(' ');
 
-    //master switch for bot commands
-    switch (true) {
+    //master switch for bot commands (remember to make them all caps for case insensitivity)
+    switch (msgArr[1].toUpperCase()) {
       //say hi
-      case (/hello/i.test(msgArr[1])):
+      case ('HELLO'):
         msg.channel.send('Hello!');
       break;
       //help command
-      case (/help/i.test(msgArr[1])):
+      case ('HELP'):
         help.message(msg, callout);
       break;
       //setup command
-      case (/setup/i.test(msgArr[1])):
+      case ('SETUP'):
         setup.message(msg, callout);
       break;
       //initialize guild doc in DB
-      case (/init/i.test(msgArr[1])):
+      case ('INIT'):
         init.initialize(msg);
       break;
       //sets cooldown on post credit
-      case (/set-cooldown/i.test(msgArr[1])):
+      case ('SET-COOLDOWN'):
         set.cooldown(msg, msgArr);
       break;
       //sets posts to advance to granted role
-      case (/set-posts/i.test(msgArr[1])):
+      case ('SET-POSTS'):
         set.posts(msg, msgArr);
       break;
       //sets days user must be in the server for granted role
-      case (/set-days/i.test(msgArr[1])):
+      case ('SET-DAYS'):
         set.days(msg, msgArr);
       break;
       //set bot command channel
-      case (/bot-channel/i.test(msgArr[1])):
+      case ('BOT-CHANNEL'):
         set.channel(msg, msgArr, 'botChannel');
       break;
       //set greeting channel
-      case (/greeting-channel/i.test(msgArr[1])):
+      case ('GREETING-CHANNEL'):
         set.channel(msg, msgArr, 'greetingChannel');
       break;
       //set rules channel
-      case (/rules-channel/i.test(msgArr[1])):
+      case ('RULES-CHANNEL'):
         set.channel(msg, msgArr, 'rulesChannel');
       break;
       //set role to give members on join
-      case (/starting-role/i.test(msgArr[1])):
+      case ('STARTING-ROLE'):
         set.role(msg, msgArr, 'startingRole');
       break;
       //set role to be granted after activity
-      case (/granted-role/i.test(msgArr[1])):
+      case ('GRANTED-ROLE'):
         set.role(msg, msgArr, 'grantedRole');
       break;
       //set role for muzzle
-      case (/muzzle-role/i.test(msgArr[1])):
+      case ('MUZZLE-ROLE'):
         set.role(msg, msgArr, 'muzzleRole');
       break;
       //muzzle command
-      case (/muzzle/i.test(msgArr[1])):
+      case ('MUZZLE'):
         mod.muzzle(msg);
       break;
       //kick command
-      case (/kick/i.test(msgArr[1])):
+      case ('KICK'):
         mod.kick(msg);
       break;
       //ban command
-      case (/ban/i.test(msgArr[1])):
+      case ('BAN'):
         mod.ban(msg);
       break;
     };
